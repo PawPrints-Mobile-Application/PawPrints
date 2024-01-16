17ace97774b86814eaf7c53daa682c92e5571614
Lex <johnlexterpanti@gmail.com>
2024-01-17 01:06:57 +0800

    bug fixes

--- src/components/modals/ModalAddLog.vue
+++ src/components/modals/ModalAddLog.vue
@@ -239,7 +239,7 @@ const Process = () => {
       ).then(() => {
         state.processing = false;
         PawprintsEvent.EventDispatcher("modal-add-log", "hide");
-        emit("success", {propsLAD: data, date: });
+        emit("success");
       }),
     1500
   );
