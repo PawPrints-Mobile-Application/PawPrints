00eff0242a95c8edef35299e1e769bab84b587b5
Lex <johnlexterpanti@gmail.com>
2024-01-18 05:54:56 +0800

    profile changes

--- src/views/settings/Profile.vue
+++ src/views/settings/Profile.vue
@@ -7,20 +7,29 @@
         <InputText placeholder="Username" v-model="form.username" />
       </InputWrapper>
       <InputWrapper label="Email">
-        <InputText placeholder="Email" v-model="form.email" freeze />
+        <InputText
+          placeholder="Email"
+          :modelValue="UserInfo.GetEmail()"
+          freeze
+        />
       </InputWrapper>
-      <InputWrapper label="Password"> </InputWrapper>
-      <ButtonDanger class="button-reset" value="Reset Password" />
+      <InputWrapper label="Password">
+        <ButtonDanger
+          class="button-reset"
+          value="Reset Password"
+          @click="ResetPassword"
+        />
+      </InputWrapper>
 
       <div class="buttons">
-        <ButtonDanger value="Clear" />
-        <ButtonSuccess value="Save" />
+        <ButtonDanger value="Clear" @click="Clear" :disabled="disableButton" />
+        <ButtonSuccess value="Save" @click="Save" :disabled="disableButton" />
       </div>
     </main>
   </LayoutPage>
 </template>
 <script setup lang="ts">
-import { reactive } from "vue";
+import { computed, reactive, ref } from "vue";
 import {
   ButtonDanger,
   ButtonSuccess,
@@ -29,19 +38,67 @@ import {
   InputWrapper,
 } from "../../components";
 import { LayoutHeader, LayoutPage } from "../../layout";
-import { UserInfo } from "../../utils";
+import { Themes, UserInfo } from "../../utils";
+import {
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  updateProfile,
+} from "firebase/auth";
+import auth from "../../server/firebase";
+import { Set } from "../../server/models/Information";
 
-const defaultValues = {
+const defaultValues = reactive({
   username: UserInfo.GetUsername(),
-  email: UserInfo.GetEmail(),
   avatar: UserInfo.GetAvatar(),
-};
+});
 
 const form = reactive({
   username: defaultValues.username,
-  email: defaultValues.email,
   avatar: defaultValues.avatar,
 });
+
+const Clear = () => {
+  form.username = defaultValues.username;
+  form.avatar = defaultValues.avatar;
+};
+
+const disableButton = computed(
+  () =>
+    form.username === defaultValues.username &&
+    form.avatar === defaultValues.avatar
+);
+
+const user = ref();
+onAuthStateChanged(auth, (currentUser) => (user.value = currentUser));
+
+const Save = () => {
+  UserInfo.SetAvatar(form.avatar);
+  UserInfo.SetUsername(form.username);
+  defaultValues.avatar = form.avatar;
+  defaultValues.username = form.username;
+  return Set({
+    uid: UserInfo.GetUID()!,
+    email: UserInfo.GetEmail()!,
+    username: form.username,
+    subscription: UserInfo.GetSubscription()!,
+    theme: Themes.Get().theme,
+    mode: Themes.Get().mode,
+    avatar: form.avatar,
+  })
+    .then(() => updateProfile(user.value, { displayName: form.username }))
+    .then(() => {
+      console.log("Profile updated successfully");
+    });
+};
+
+const ResetPassword = () =>
+  sendPasswordResetEmail(auth, UserInfo.GetEmail())
+    .then(() => {
+      console.log("Password Reset Sent.");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 </script>
 <style scoped>
 main {
