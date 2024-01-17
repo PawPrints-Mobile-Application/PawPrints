f838076895ca994bc6b869135576463d708e3b1c
Alliah Nhoree Galamiton <alliah1210@gmail.com>
2024-01-18 03:02:09 +0800

    landmarks schedule

--- src/utils/Landmarks.ts
+++ src/utils/Landmarks.ts
@@ -1,31 +1,14 @@
-type LandmarkData = {
-  name: string;
-  schedule?: {
-    Monday?: string;
-    Tuesday?: string;
-    Wednesday?: string;
-    Thursday?: string;
-    Friday?: string;
-    Saturday?: string;
-    Sunday?: string;
-    All?: string;
-  };
-  address: string;
-  contact?: string;
-  coords: { x: number; y: number };
-};
-
 const entries = {
   "Cayco Animal Clinic": {
     name: "Cayco Animal Clinic",
-    schedule: { All: "9:00 AM - 5:00 PM" },
+    schedule: "Monday - Sunday: 9:00 AM - 5:00 PM",
     address: "434 Cayco St, Sampaloc, Manila, 1008 Metro Manila",
     contact: "09420517675",
     coords: { x: 14.603857380447206, y: 120.99634697040814 },
   },
   "Estacion Animales Clinica Veterinaria Otis": {
     name: "Estacion Animales Clinica Veterinaria Otis",
-    schedule: { All: "9:00 AM - 9:00 PM" },
+    schedule: "Monday - Sunday: 9:00 AM - 9:00 PM",
     address:
       "G/Ground Floor, Topmark Building, 1763 M Guazon Street, 664-A Paco, Manila, 1007 Metro Manila",
     contact: "3707437",
@@ -33,19 +16,21 @@ const entries = {
   },
   "Your Vets Animal Clinic": {
     name: "Your Vets Animal Clinic",
+    schedule: "Monday - Sunday: 9:00 AM - 9:00 PM",
     address: "2009 Gerardo Tuazon St, Sampaloc, Manila, 1008 Metro Manila",
+    contact: "0947 810 6919",
     coords: { x: 14.609230126595762, y: 121.0037478516361 },
   },
   "WT Animal Wellness & Veterinary Clinic": {
     name: "WT Animal Wellness & Veterinary Clinic",
-    schedule: { All: "9:00 AM - 6:00 PM" },
+    schedule: "Monday - Sunday: 9:00 AM - 6:00 PM",
     address: "1971 Gerardo Tuazon St, Sampaloc, Manila, 1008 Metro Manila",
     contact: "09360592621",
     coords: { x: 14.608410476522158, y: 121.00359104108378 },
   },
   "Pets Avenue Animal Wellness Center": {
     name: "Pets Avenue Animal Wellness Center",
-    schedule: { All: "9:00 AM - 6:00 PM" },
+    schedule: "Monday - Sunday: 9:00 AM - 6:00 PM",
     address:
       "3574, 1006 Buenos Aires Street, Sampaloc, Manila, 1006 Metro Manila",
     contact: "09568521191",
@@ -53,15 +38,7 @@ const entries = {
   },
   "Santa Ana Animal Health Clinic": {
     name: "Santa Ana Animal Health Clinic",
-    schedule: {
-      Monday: "9:00 AM - 6:00 PM",
-      Tuesday: "9:00 AM - 6:00 PM",
-      Wednesday: "9:00 AM - 6:00 PM",
-      Thursday: "9:00 AM - 6:00 PM",
-      Friday: "9:00 AM - 6:00 PM",
-      Saturday: "9:00 AM - 6:00 PM",
-      Sunday: "9:00 AM - 4:00 PM",
-    },
+    schedule: "Monday - Saturday: 9:00 AM - 6:00 PM, Sunday: 9:00 AM - 4:00 PM",
     address:
       "CFA Building, 2259 Calderon, Santa Ana, Manila, 1009 Metro Manila",
     contact: "0282417224",
@@ -69,22 +46,15 @@ const entries = {
   },
   "Canine Veterinary Clinic": {
     name: "Canine Veterinary Clinic",
-    schedule: {
-      Monday: "8:30 AM - 5:00 PM",
-      Tuesday: "8:30 AM - 5:00 PM",
-      Wednesday: "8:30 AM - 5:00 PM",
-      Thursday: "8:30 AM - 5:00 PM",
-      Friday: "8:30 AM - 5:00 PM",
-      Saturday: "8:30 AM - 5:00 PM",
-      Sunday: "8:30 AM - 11:00 AM",
-    },
+    schedule:
+      "Monday - Saturday: 8:30 AM - 5:00 PM, Sunday: 8:30 AM - 11:00 AM",
     address: "11-A Araneta Ave, Quezon City, 1113 Metro Manila",
     contact: "0287165257",
     coords: { x: 14.606637220739035, y: 121.01753150905142 },
   },
   "Wags & Purrs Veterinary Clinic": {
     name: "Wags & Purrs Veterinary Clinic",
-    schedule: { All: "9:00 AM - 6:00 PM" },
+    schedule: "Monday - Sunday: 9:00 AM - 6:00 PM",
     address:
       "2nd floor, One Square Bldg, 3340 V. Mapa Ext, Santa Mesa, Manila, 1016 Metro Manila",
     contact: "09763414439",
@@ -92,44 +62,29 @@ const entries = {
   },
   "My Pet Doctor": {
     name: "My Pet Doctor",
-    schedule: {
-      Monday: "9:00 AM - 5:00 PM",
-      Tuesday: "9:00 AM - 5:00 PM",
-      Wednesday: "Closed",
-      Thursday: "9:00 AM - 5:00 PM",
-      Friday: "9:00 AM - 5:00 PM",
-      Saturday: "9:00 AM - 5:00 PM",
-      Sunday: "9:00 AM - 5:00 PM",
-    },
+    schedule:
+      "Monday - Sunday (except Wednesday): 9:00 AM - 5:00 PM, Wednesday: Closed",
     address: "59, 1500 N. Domingo, San Juan, Metro Manila",
     contact: "0279437368",
     coords: { x: 14.604739349525676, y: 121.02234691713905 },
   },
   "Bark and Meow Pet Shop and Veterinary Clinic": {
     name: "Bark and Meow Pet Shop and Veterinary Clinic",
-    schedule: { All: "Open 24 hours" },
+    schedule: "Monday - Sunday: Open 24 hours",
     address: "J24G+WJH, triple a bldg, N. Domingo, San Juan, Metro Manila",
     contact: "09219271069",
     coords: { x: 14.607389280795996, y: 121.02659791406019 },
   },
   "The Pet Project Vet Clinic": {
     name: "The Pet Project Vet Clinic",
-    schedule: { All: "10:00 AM - 6:00 PM" },
+    schedule: "Monday - Sunday: 10:00 AM - 6:00 PM",
     address: "16 Regidor, San Juan, 1500 Metro Manila",
     contact: "0275085838",
     coords: { x: 14.602834150390638, y: 121.02724007868645 },
   },
   "MandaVets Animal Clinic": {
     name: "MandaVets Animal Clinic",
-    schedule: {
-      Monday: "8:30 AM - 4:30 PM",
-      Tuesday: "8:30 AM - 4:30 PM",
-      Wednesday: "8:30 AM - 4:30 PM",
-      Thursday: "8:30 AM - 4:30 PM",
-      Friday: "8:30 AM - 4:30 PM",
-      Saturday: "8:30 AM - 4:30 PM",
-      Sunday: "Closed",
-    },
+    schedule: "Monday - Saturday: 8:30 AM - 4:30 PM, Sunday: Closed",
     address:
       "Unit No. 4A, Ground Floor, Gomega 1 Condominium, 353 P. Martinez, Mandaluyong, 1550 Metro Manila",
     contact: "09954676499",
@@ -137,20 +92,21 @@ const entries = {
   },
   "Immanuel Animal Clinic Pet Supplies & Grooming Center": {
     name: "Immanuel Animal Clinic Pet Supplies & Grooming Center",
+    schedule: "Monday - Sunday: 10:00 AM - 6:00 PM",
     address: "196 Rt. Rev. G. Aglipay, Mandaluyong, Metro Manila",
     contact: "09311318670",
     coords: { x: 14.587527863155252, y: 121.02466758740886 },
   },
   "Curapet Animal Clinic": {
     name: "Curapet Animal Clinic",
-    schedule: { All: "Open 24 hours" },
+    schedule: "Monday - Sunday: Open 24 hours",
     address: "2543 Tejeron St, Santa Ana, Manila, 1009 Metro Manila",
     contact: "0282531668",
     coords: { x: 14.57614558034792, y: 121.01177928782833 },
   },
   "Wags & Tails Veterinary Clinic": {
     name: "Wags & Tails Veterinary Clinic",
-    schedule: { All: "Open 24 hours" },
+    schedule: "Monday - Sunday: Open 24 hours",
     address:
       "U8,Creekside Square Bldg 74G/F, 74 Tomas Morato Ave, Quezon City, 1112 Metro Manila",
     contact: "0277458131",
@@ -158,13 +114,14 @@ const entries = {
   },
   "Shepherd Animal Clinic": {
     name: "Shepherd Animal Clinic",
+    schedule: "Monday - Sunday: 10:00 AM - 6:00 PM",
     address: "1427 Pedro Gil St, Corner Penafrancia, Paco",
     contact: "0443074809",
     coords: { x: 14.579467535450616, y: 120.99769762590327 },
   },
   "Biyaya Animal Care": {
     name: "Biyaya Animal Care",
-    schedule: { All: "Open 24 hours" },
+    schedule: "Monday - Sunday: Open 24 hours",
     address:
       "Mandala Park, 312 Shaw Boulevard, Brgy, Pleasant Hills, Mandaluyong, 1550 Metro Manila",
     contact: "09171420171",
@@ -172,43 +129,28 @@ const entries = {
   },
   "Blessed Veterinary Clinic": {
     name: "Blessed Veterinary Clinic",
-    schedule: { All: "Open 24 hours" },
+    schedule: "Monday - Sunday: Open 24 hours",
     address: "281-C Fernando Poe Jr. Ave, Quezon City, 1105 Metro Manila",
     contact: "0289219634",
     coords: { x: 14.651924736419948, y: 121.01732384236178 },
   },
   "Citivet Veterinary Clinic & Pet Supply Co": {
     name: "Citivet Veterinary Clinic & Pet Supply Co",
-    schedule: {
-      Monday: "9:00 AM - 6:00 PM",
-      Tuesday: "9:00 AM - 6:00 PM",
-      Wednesday: "9:00 AM - 6:00 PM",
-      Thursday: "9:00 AM - 6:00 PM",
-      Friday: "9:00 AM - 6:00 PM",
-      Saturday: "9:00 AM - 6:00 PM",
-      Sunday: "9:00 AM - 3:00 PM",
-    },
+    schedule: "Monday - Saturday: 9:00 AM - 6:00 PM, Sunday: 9:00 AM - 3:00 PM",
     address: "1913 Dapitan St, Sampaloc, Manila, 1008 Metro Manila",
     contact: "09192532080",
     coords: { x: 14.617026652725393, y: 120.99379558043672 },
   },
   "Cubao Animal Clinic": {
     name: "Cubao Animal Clinic",
-    schedule: { All: "Open 24 hours" },
+    schedule: "Monday - Sunday: Open 24 hours",
     address: "915 Aurora Blvd, Quezon City, 1102 Metro Manila",
+    contact: "0995 916 8123",
     coords: { x: 14.62620611966153, y: 121.05855411056167 },
   },
   "Fur Bloom Animal Clinic": {
     name: "Fur Bloom Animal Clinic",
-    schedule: {
-      Monday: "9:00 AM - 6:00 PM",
-      Tuesday: "9:00 AM - 6:00 PM",
-      Wednesday: "9:00 AM - 6:00 PM",
-      Thursday: "9:00 AM - 6:00 PM",
-      Friday: "9:00 AM - 6:00 PM",
-      Saturday: "9:00 AM - 6:00 PM",
-      Sunday: "9:00 AM - 6:00 PM",
-    },
+    schedule: "Monday - Sunday: 9:00 AM - 6:00 PM",
     address:
       "Unit 7, BSA Commercial Center, Shaw Blvd, Mandaluyong, 1550 Metro Manila",
     contact: "09951048747",
@@ -216,15 +158,8 @@ const entries = {
   },
   "E. A. Garcia Veterinary Clinic": {
     name: "E. A. Garcia Veterinary Clinic",
-    schedule: {
-      Monday: "9:00 AM - 6:00 PM",
-      Tuesday: "9:00 AM - 6:00 PM",
-      Wednesday: "9:00 AM - 6:00 PM",
-      Thursday: "9:00 AM - 6:00 PM",
-      Friday: "9:00 AM - 6:00 PM",
-      Saturday: "9:00 AM - 6:00 PM",
-      Sunday: "9:00 AM - 12:00 PM",
-    },
+    schedule:
+      "Monday - Saturday: 9:00 AM - 6:00 PM, Sunday: 9:00 AM - 12:00 PM",
     address:
       "Unit 1-A, Dolmar Building, United Nations Ave, Paco, Manila, 1007 Metro Manila",
     contact: "0285246290",
@@ -232,85 +167,45 @@ const entries = {
   },
   "PPBCC Veterinary Hospital": {
     name: "PPBCC Veterinary Hospital",
-    schedule: { All: "Open 24 hours" },
+    schedule: "Monday - Sunday: 9:00 AM - 6:00 PM",
     address: "102 Primo Cruz, Mandaluyong, 1550 Metro Manila",
+    contact: "0985 179 5495",
     coords: { x: 14.587694488700814, y: 121.03078737652292 },
   },
   "MEVC Manila East Veterinary Care": {
     name: "MEVC Manila East Veterinary Care",
-    schedule: {
-      Monday: "10:00 AM - 7:00 PM",
-      Tuesday: "10:00 AM - 7:00 PM",
-      Wednesday: "10:00 AM - 7:00 PM",
-      Thursday: "10:00 AM - 7:00 PM",
-      Friday: "10:00 AM - 7:00 PM",
-      Saturday: "10:00 AM - 7:00 PM",
-      Sunday: "Closed",
-    },
+    schedule: "Monday - Saturday: 10:00 AM - 7:00 PM, Sunday: Closed",
     address: "71-73 E Rodriguez Sr. Ave, Quezon City, 1113 Metro Manila",
     contact: "09611182230",
     coords: { x: 14.621748005598295, y: 121.01087465799229 },
   },
   "Fibo and Nesta Animal Clinic and Grooming Services": {
     name: "Fibo and Nesta Animal Clinic and Grooming Services",
-    schedule: {
-      Monday: "8:00 AM - 5:00 PM",
-      Tuesday: "8:00 AM - 5:00 PM",
-      Wednesday: "8:00 AM - 5:00 PM",
-      Thursday: "8:00 AM - 5:00 PM",
-      Friday: "8:00 AM - 5:00 PM",
-      Saturday: "8:00 AM - 5:00 PM",
-      Sunday: "8:00 AM - 5:00 PM",
-    },
+    schedule: "Monday - Sunday: 8:00 AM - 5:00 PM",
     address: "Barangay A. Luna, Mandaluyong, Metro Manila",
     contact: "0282807892",
     coords: { x: 14.59234601165355, y: 121.02666750372347 },
   },
   "Vetfront Animal Wellness Center": {
     name: "Vetfront Animal Wellness Center",
-    schedule: {
-      Monday: "8:00 AM - 8:00 PM",
-      Tuesday: "8:00 AM - 8:00 PM",
-      Wednesday: "8:00 AM - 8:00 PM",
-      Thursday: "8:00 AM - 8:00 PM",
-      Friday: "8:00 AM - 8:00 PM",
-      Saturday: "8:00 AM - 8:00 PM",
-      Sunday: "8:00 AM - 8:00 PM",
-    },
+    schedule: "Monday - Sunday: 8:00 AM - 8:00 PM",
     address: "1764 Laong Laan Rd, Sampaloc, Manila, 1008 Metro Manila",
     contact: "09278353189",
     coords: { x: 14.616930999154222, y: 120.99113360082828 },
   },
   "Vets On The Block Pet Clinic": {
     name: "Vets On The Block Pet Clinic",
-    schedule: {
-      Monday: "10:00 AM - 5:00 PM",
-      Tuesday: "10:00 AM - 5:00 PM",
-      Wednesday: "10:00 AM - 5:00 PM",
-      Thursday: "10:00 AM - 5:00 PM",
-      Friday: "10:00 AM - 5:00 PM",
-      Saturday: "10:00 AM - 5:00 PM",
-      Sunday: "10:00 AM - 5:00 PM",
-    },
+    schedule: "Monday - Sunday: 10:00 AM - 5:00 PM",
     address: "10 Hyacinth St, Quezon City, 1103 Metro Manila",
     contact: "0286822931",
     coords: { x: 14.629846671510835, y: 121.02073076115374 },
   },
   "Vets In Practice": {
     name: "Vets In Practice",
-    schedule: {
-      Monday: "9:00 AM - 7:00 PM",
-      Tuesday: "9:00 AM - 7:00 PM",
-      Wednesday: "9:00 AM - 7:00 PM",
-      Thursday: "9:00 AM - 7:00 PM",
-      Friday: "9:00 AM - 7:00 PM",
-      Saturday: "9:00 AM - 7:00 PM",
-      Sunday: "9:00 AM - 7:00 PM",
-    },
+    schedule: "Monday - Sunday: 9:00 AM - 7:00 PM",
     address: "63 Maysilo Cir, Mandaluyong, 1550 Metro Manila",
     contact: "0285311581",
     coords: { x: 14.581042114970572, y: 121.03181730996661 },
   },
 };
-export type { LandmarkData };
 export default { entries };
